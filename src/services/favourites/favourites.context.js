0dba40ee15f98ee3364a241b8dee147b8505b7f6
Akshita Pathak <pathakshita07@gmail.com>
2025-01-17 12:17:24 +0530

    storing favourites in async phone storage

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,10 +1,42 @@
-﻿import React, { createContext, useState } from "react"
+﻿import React, { createContext, useState, useEffect } from "react"
+// npx expo install @react-native-async-storage/async-storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
 
     const [favourites, setFavourites] = useState([]);      //empty array initially as we dont have favourites when we initially load our apps
+
+    // STORING OUR FAVOURITES ON OUR PHONE:
+    const saveFavourites = async (value) => {
+        try {
+            const jsonValue = JSON.stringify(value);
+            await AsyncStorage.setItem('@favourites', jsonValue);
+        } catch (e) {
+            console.log("Error storing favourites: ", e)
+        }
+    };
+
+    const loadFavourites = async () => {
+        try {
+            const value = await AsyncStorage.getItem('@favourites');
+            if (value !== null) {
+                setFavourites(JSON.parse(value))
+            }
+        } catch (e) {
+            console.log("Error loading favourites: ", e)
+        }
+    };
+
+    // to load the initial favourites:
+    useEffect(() => {
+        loadFavourites()
+    }, [])
+
+    useEffect(() => {
+        saveFavourites(favourites)
+    }, [favourites])
 
     // adding a restaurant to our favourites:
     const add = (restaurant) => {
