036cfea0dbc069b07e7c9c48f51db44c98f9a481
Akshita Pathak <pathakshita07@gmail.com>
2025-03-24 18:55:14 +0530

    account login page with bg img

---
+++ src/features/account/screens/account.screen.js
@@ -0,0 +1,11 @@
+﻿import React from "react";
+import { ImageBackground } from 'react-native';
+
+import { AccountBackground } from '../components/account.styles';
+
+export const AccountScreen = () => {
+    return (
+        <AccountBackground>
+        </AccountBackground>
+    )
+}
---
+++ src/features/account/screens/register.screen.js
@@ -0,0 +1,9 @@
+﻿import React from "react";
+import { AccountBackground } from '../components/account.styles';
+
+export const RegisterScreen = () => {
+    return (
+        <AccountBackground>
+        </AccountBackground>
+    )
+}
