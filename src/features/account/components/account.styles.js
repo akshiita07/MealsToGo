3bb03223f09610ba484ea120d2b7e006109b4f8c
Akshita Pathak <pathakshita07@gmail.com>
2025-03-24 19:29:33 +0530

    buttons added

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,4 +1,6 @@
 ﻿import styled from "styled-components/native";
+import { colors } from "../../../infrastructure/theme/colors";
+import { Button } from "react-native-paper";
 
 export const AccountBackground = styled.ImageBackground.attrs({
     source: require("../../../../assets/home_bg.jpg"),
@@ -9,8 +11,24 @@ export const AccountBackground = styled.ImageBackground.attrs({
 `;
 
 export const AccountCover = styled.View`
+display: flex;
+justify-content: center;
+align-items: center;
     position: absolute;
     width: 100%;
     height: 100%;
     background-color: rgba(255, 255, 255, 0.2);
+`;
+
+// add content:
+export const AccountContainer = styled.View`
+    background-color: rgba(255, 255, 255, 0.7);
+    padding: ${(props) => props.theme.space[4]};
+`;
+
+// add buttons:
+export const AuthButton = styled(Button).attrs({
+    color: colors.brand.primary,
+})`
+padding: ${(props) => props.theme.space[1]};
 `;
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,14 +1,31 @@
 ﻿import React from "react";
 import { ImageBackground } from 'react-native';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
-import { AccountBackground } from '../components/account.styles';
-import { AccountCover } from '../components/account.styles';
+import { AccountBackground, AccountCover, AccountContainer, AuthButton } from '../components/account.styles';
 
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
     return (
         <AccountBackground>
             <AccountCover>
-
+                <AccountContainer>
+                    <AuthButton icon="lock-open-outline" mode="contained" onPress={() => {
+                        console.log("Login Button Pressed");
+                        navigation.navigate("Login");
+                    }
+                    }>
+                        Login
+                    </AuthButton>
+                    <Spacer size="large">
+                        <AuthButton icon="lock-open-outline" mode="contained" onPress={() => {
+                            console.log("Register Button Pressed");
+                            navigation.navigate("Register");
+                        }
+                        }>
+                            Register
+                        </AuthButton>
+                    </Spacer>
+                </AccountContainer>
             </AccountCover>
         </AccountBackground>
     )
