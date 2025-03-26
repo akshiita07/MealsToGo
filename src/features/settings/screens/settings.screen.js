e3b91c29ceef5f1e51edceda1527de9195db6228
Akshita Pathak <pathakshita07@gmail.com>
2025-03-26 19:06:11 +0530

    settings page modified

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,36 +1,52 @@
 ﻿import React, { useContext } from "react";
 import { SafeAreaView } from 'react-native';
-import { Button, List } from "react-native-paper";
+import { Avatar, List } from "react-native-paper";
+import { Text } from "../../../components/typography/text.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
+import { SettingsItem, AvatarContainer } from "../components/settings.style";
 
 export const SettingsScreen = ({ navigation }) => {
-    const { onLogout } = useContext(AuthenticationContext);
+    const { onLogout, user } = useContext(AuthenticationContext);
 
     return (
         <SafeAreaView >
+            <AvatarContainer>
+                <Avatar.Icon
+                    size={150}
+                    icon="human"
+                    backgroundColor="#2182BD"
+                >
+
+                </Avatar.Icon>
+
+                <Spacer position="top" size="large">
+                    <Text variant="label" style={{ textAlign: 'center' }}>
+                        Welcome
+                    </Text>
+                    <Text variant="label" style={{ color: "#2182BD" }}>
+                        {user.email}
+                    </Text>
+                </Spacer>
+            </AvatarContainer>
+
             <List.Section>
                 {/* add favourites button */}
-                <List.Item
-                    style={{
-                        padding: 16
-                    }}
+                <SettingsItem
                     title="Favourites"
                     description="View your favourites"
                     left={(props) => <List.Icon {...props} color="black" icon="heart" />}
                     onPress={() => { console.log("User wants to view favourites"); navigation.navigate("Favourites"); }}>
-                </List.Item>
+                </SettingsItem>
 
                 {/* add logout button */}
-                <List.Item
-                    style={{
-                        padding: 16
-                    }}
+                <SettingsItem
                     title="Logout"
                     left={(props) => <List.Icon {...props} color="black" icon="logout" />}
                     onPress={() => { console.log("User wants to log out"); onLogout(); }}>
-                </List.Item>
+                </SettingsItem>
             </List.Section>
 
         </SafeAreaView >
